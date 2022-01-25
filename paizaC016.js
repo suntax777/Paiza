f2ee561248b788c7da05b695f2286564d48313af
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-26 07:40:18 +0900

    firt commit

--- paizaC016.js
+++ paizaC016.js
@@ -16,31 +16,33 @@ reader.on("close", () => {
   //console.log(inputList);
   const count = inputList.length;
   //console.log(count);
-  const leeString = { A: 4, E: 3, G: 6, I: 1, O: 0, S: 5, Z: 2 };
+  //const leeString = {A:4, E:3, G:6, I:1, O:0, S:5, Z:2};
   //console.log(leeString);
-  const outputList = [];
+  var outputList = [];
+  //outputList[0] = 4;
+  //console.log(outputList);
   //console.log(outputList);
   //console.log(inputList[0])
   for (var i = 0; i < count; i++) {
     //console.log(i);
     if (inputList[i] === "A") {
-      outputList.push[i] === "4";
+      outputList[i] = "4";
       //console.log(outputList[i]);
       console.log(outputList);
     } else if (inputList[i] === "E") {
-      outputList.push[i] === "3";
+      outputList[i] = "3";
     } else if (inputList[i] === "G") {
-      outputList.push[i] === "6";
+      outputList[i] = "6";
     } else if (inputList[i] === "I") {
-      outputList.push[i] === "1";
+      outputList[i] = "1";
     } else if (inputList[i] === "O") {
-      outputList.push[i] === "0";
+      outputList[i] = "0";
     } else if (inputList[i] === "S") {
-      outputList.push[i] === "5";
+      outputList[i] = "5";
     } else if (inputList[i] === "Z") {
-      outputList.push[i] === "2";
+      outputList[i] = "2";
     } else {
-      inputList[i] === outputList.push[i];
+      outputList[i] = inputList[i];
     }
   }
   console.log(outputList);
